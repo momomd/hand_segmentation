beab8ac92f9ba952410ae971f0a77c3c2553b649
Uran Oh <uranoh@umiacs.umd.edu>
2014-06-03 15:55:34 -0400

    Initial Commit

---
+++ hand_segmentation/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  hand_segmentation
+//
+//  Created by Uran Oh on 6/3/14.
+//  Copyright (c) 2014 Uran Oh. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
